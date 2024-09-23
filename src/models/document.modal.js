fe685027e65b247e6d3d9c5fedbeff0e7aa28979
Madhav <madhav.shar06ma@gmail.com>
2024-09-24 01:15:58 +0530

    added user and document modal

---
+++ src/models/document.modal.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const documentSchema = new mongoose.Schema({
+    content: {
+        type: String
+    },
+    owner: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
+    },
+    sharedWith:{
+        type:mongoose.Schema.ObjectId,
+        ref:"User"
+    }
+}, { timestamps: true })
+
+export const Document = mongoose.model("Document",documentSchema);
