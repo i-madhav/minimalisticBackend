ffaa06ffaab833d4a7408c2c404c32a990b69839
i-madhav <madhav.shar06ma@gmail.com>
2024-09-29 21:14:58 +0530

    completed save document controller

--- src/contoller/document.controller.js
+++ src/contoller/document.controller.js
@@ -4,17 +4,17 @@ import ApiResponse from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/AsyncHandler.js";
 
 const generateDocument = asyncHandler(async (req, res) => {
-    const { id, content, userid } = req.body;
+    const { id, userid } = req.body;
 
     if (userid && !id) {
         // document creation for logged-in user
         const document = await Document.create({
-            content:" ",
-            owner:userid
+            content: " ",
+            owner: userid
         });
 
-        if(!document) throw new ApiError(500 , "unable to create document");
-        return res.status(200).json(200,"document created successfully for logged-in user",  document);
+        if (!document) throw new ApiError(500, "unable to create document");
+        return res.status(200).json(200, "document created successfully for logged-in user", document);
 
     } else if (!userid && !id) {
         // user is not logged in and is trying to create a document
@@ -30,20 +30,29 @@ const generateDocument = asyncHandler(async (req, res) => {
     }
 });
 
-const saveDocument = async(id , userid , content , sharedWith) => {
-    if (id && userid && sharedWith) {
+const saveDocument = async (id, userid, content, sharedWith) => {
+    if (id && userid && sharedWith.length > 0) {
         // for logged in and he has restricted document to shared only
         // for save document
-
-
-    } else if (id && content){
+        const documentContent = await Document.findById(id);
+        if (!documentContent) throw new ApiError(404, "Document not found");
+        const isOwner = documentContent.owner.equals(userid);
+        const isSharedWith = documentContent.sharedWith.some((item) => item.equals(userid));
+        if (isOwner || isSharedWith) {
+            documentContent.content = content;
+            await documentContent.save({ validateBeforeSave: false });
+            return documentContent;
+        }else{
+            return "You do not have access to this document"
+        }
+    } else if (id && content) {
         // for non logged-in user and logged user too if document is not in sharedwith mode 
         // save document
         const dataContent = await Document.findById(id);
-        if(!dataContent) throw new ApiError(404 , "couldn't find the document");
+        if (!dataContent) throw new ApiError(404, "couldn't find the document");
 
         dataContent.content = content;
-        await dataContent.save({validateBeforeSave:false});
+        await dataContent.save({ validateBeforeSave: false });
         return dataContent;
     }
 };
@@ -56,14 +65,14 @@ const fetchDocument = asyncHandler(async (req, res) => {
 
     } else if (type === "NonLoggedInUser") {
         const { id } = req.body;
-        
+
     }
 });
 
-const numberDocumentUserCreated = asyncHandler(async(req , res) => {
-  const docs = await Document.find({owner:req.user._id}).populate('owner');
-  return res.status(200).json(new ApiResponse(200 , "Number Document Fetched") , docs)
+const numberDocumentUserCreated = asyncHandler(async (req, res) => {
+    const docs = await Document.find({ owner: req.user._id }).populate('owner');
+    return res.status(200).json(new ApiResponse(200, "Number Document Fetched"), docs)
 })
 
 
-export {generateDocument , saveDocument , fetchDocument , numberDocumentUserCreated}
+export { generateDocument, saveDocument, fetchDocument, numberDocumentUserCreated }
