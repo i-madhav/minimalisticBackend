476282d26344e02965d7898f5cea21ebbea886e0
i-madhav <madhav.shar06ma@gmail.com>
2024-10-26 18:48:05 +0530

    socket.js changes

--- src/Socket/Socket.js
+++ src/Socket/Socket.js
@@ -1,15 +1,34 @@
 import { saveDocument } from "../contoller/document.controller.js"
 export async function handleSocketDocument(io) {
+    const connectedClients = new Map();
+
     io.on('connection' , (socket) => {
+
+        socket.on("joinDocument", (docid) => {
+            socket.join(docid);
+            
+            if (!connectedClients.has(docid)) {
+                connectedClients.set(docid, new Set());
+            }
+            connectedClients.get(docid).add(socket.id);
+            
+            // Notify others about new connection
+            socket.to(docid).emit("userJoined", {
+                connectedUsers: connectedClients.get(docid).size
+            });
+            console.log("Connected client");
+            console.log(connectedClients.get(docid).size);
+        });
+
         socket.on('updatedDataFromTheClient' , async (data) => {
             try {
                 const response = await saveDocument(data.id , data.userid , data.content , data.sharedWith);
-                socket.broadcast.emit("serverResponse" , {
+                socket.to(data.id).emit("serverResponse" , {
                     status:200,
                     document:response
                 });
             } catch (error) {
-                socket.emit("serverResponse", {
+                socket.to(data.id).emit("serverResponse", {
                     status:500,
                     document:error 
                 })
